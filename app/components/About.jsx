b1ad9602aa99aff8b67e0be432077c3b17a9f4f7
Angelina Cole <acole@thoughtworks.com>
2016-11-27 14:09:27 -0600

    when you have two neighboring react components, they better be wrapped in a div

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,10 @@ var React = require('react');
 var About = (props) => {
   //use when we're not managing state
   return(
-    <h3>About component</h3>
-    <p>Welcome to the about page!</p>
+    <div>
+      <h3>About component</h3>
+      <p>Welcome to the about page!</p>
+    </div>
     )
 };
 
