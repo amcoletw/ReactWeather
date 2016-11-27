b9fa8c80be8bbb6d67ab6a298b89d8d6dacf3275
Angelina Cole <acole@thoughtworks.com>
2016-11-27 13:44:21 -0600

    try that again

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 //Create our app!
 var app = express();
-const PORT = process.env.PORT || 8000;
+var PORT = process.env.PORT || 8000;
 
 app.use(function(req, res, next) {
   if (request.headers['x-forwarded-proto'] === 'http') {
